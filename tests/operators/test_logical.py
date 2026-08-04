20cd9bc7711426034d099d2d83ac9b39c161d1dd
Ben0112358 <111197969+Ben0112358@users.noreply.github.com>
2026-08-04 23:07:46 +0200

    black and flake8 update (#18)

--- tests/operators/test_logical.py
+++ tests/operators/test_logical.py
@@ -3,7 +3,6 @@ from tests.utils.events import event_ids
 from tests.utils.assertions import assert_event_ids
 from llm_decision_spec.operators.logical import And, Or, Not
 
-
 # -------------------------
 # AND
 # -------------------------
